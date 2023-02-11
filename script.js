2bb119c808487e99e0f409b86ef5263fb02fb04f
Murilo2909 <92822901+Murilo2909@users.noreply.github.com>
2023-02-11 20:51:42 +0044

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.querySelector(".menu-btn").addEventListener("click", function(){
+    document.querySelector(".mobile-menu").classList.toggle("show")
+})
+})
